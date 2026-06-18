0e3594123cfe25bb0dec0347ed407c3777577750
Muhammad Afrizal Suhartono <94085778+Bykamri@users.noreply.github.com>
2026-06-18 16:17:28 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,10 @@
+// File: api/index.ts
 import { app } from '../src/index';
 
-export const config = {
-    runtime: 'edge'
-};
-
-export default app.handle;
+// Mengekspor setiap metode HTTP agar Vercel menggunakan standar Web API (Request -> Response)
+export const GET = app.handle;
+export const POST = app.handle;
+export const PUT = app.handle;
+export const PATCH = app.handle;
+export const DELETE = app.handle;
+export const OPTIONS = app.handle;
