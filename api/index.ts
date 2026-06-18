2cde8911616d168e1f8d7b8c306bf50f291c6d59
Bykamri <mafrizalsuhartono@gmail.com>
2026-06-18 11:56:39 +0700

    change api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,12 @@
+// File: api/index.ts
 import { app } from '../src/index';
 
+// PAKSA Vercel menggunakan Node.js Serverless (Bukan Edge)
 export const config = {
-    runtime: 'edge'
+    runtime: 'nodejs'
 };
 
+// Meneruskan request ke aplikasi Elysia
 export default async function handler(request: Request) {
     return app.handle(request);
 }
